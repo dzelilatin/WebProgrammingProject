c672aee90c4ee771c746d4017a9c77ff4ebad9a5
Dželila <dzelila.tinjak@stu.ibu.edu.ba>
2024-06-04 01:57:16 +0200

    commit

--- index.php
+++ index.php
@@ -2,18 +2,6 @@
 // Include the Flight framework autoload file
 require 'vendor/autoload.php';
 
-// Define your Flight routes and functionality
-flight::route('/', function() {
-    echo 'Hello world!';
-});
-
-// REST API documentation endpoint
-Flight::route('GET /docs.json', function(){
-    $openapi = \OpenApi\scan('routes');
-    header('Content-Type: application/json');
-    echo $openapi->toJson();
-});
-
 // Start the Flight framework
 Flight::start();
 ?>
